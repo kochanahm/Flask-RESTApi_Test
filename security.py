9adf8f6e968462707d2cff4610173b43bf49e333
kochanahm <kochanahm@gmail.com>
2021-01-17 23:50:13 +0300

    First commit, added REST Api to access items, stores and user aurhentication

---
+++ security.py
@@ -0,0 +1,12 @@
+from werkzeug.security import safe_str_cmp
+from models.user import UserModel
+
+
+def authenticate(username, password):
+  user = UserModel.find_by_username(username)
+  if user and safe_str_cmp(user.password,password): #safe_str_cmp ensures encoded string comparision safely
+    return user
+
+def identity(payload):
+  user_id=payload["identity"]
+  return UserModel.find_by_id(user_id)
