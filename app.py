d634d4cd7120947a39db88eb9e5c6b6b709a4566
kochanahm <kochanahm@gmail.com>
2021-01-19 01:24:29 +0300

    Added PostgreSQL environment and psycog2

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from security import authenticate, identity
 from flask import Flask
 from flask_restful import Api
@@ -12,7 +14,7 @@ from resources.store import Store,StoreList
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL","sqlite:///data.db")
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = "ahmet"
 api = Api(app)
